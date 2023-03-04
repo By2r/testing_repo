c8543a6bd793052fe224a24a058841acfea46861
By2r <79645102+By2r@users.noreply.github.com>
2023-03-04 14:41:40 +0600

    Testing again

--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
 #this is python code to learn how to create files in git
 print("This is to test if I can create a branch")
 print("What happens to deleted code?")
+print("Compare and pull request")
